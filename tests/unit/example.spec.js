aac2092d6e4af065f081876d7a9dc36541b53b6c
Ryo Utsunomiya <millionsnowliving@gmail.com>
2019-01-04 10:20:23 +0900

    use localVue

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,16 +1,17 @@
-import Vue from 'vue';
 import VeeValidate from 'vee-validate';
 import { expect } from 'chai'
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 
-Vue.use(VeeValidate);
+const localVue = createLocalVue();
+localVue.use(VeeValidate);
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
     const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+      propsData: { msg },
+      localVue,
     })
     expect(wrapper.text()).to.include(msg)
   })
